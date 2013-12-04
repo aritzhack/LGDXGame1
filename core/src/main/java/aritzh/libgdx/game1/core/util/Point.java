4d9cf9d7a26bf5297b3e912f4198dba490dc21ba
aritzhack <aritzhack@gmail.com>
2013-12-04 19:06:56 +0100

    + Added own simple Rectangle and Point classes, as GWT does not emulate them

--- core/src/main/java/aritzh/libgdx/game1/core/util/Point.java
+++ core/src/main/java/aritzh/libgdx/game1/core/util/Point.java
@@ -5,4 +5,27 @@ package aritzh.libgdx.game1.core.util;
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 public class Point {
+
+    private final int x, y;
+
+    public Point() {
+        this(0, 0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point translate(int dx, int dy) {
+        return new Point(this.x + dx, this.y + dy);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
--- core/src/main/java/aritzh/libgdx/game1/core/util/Rectangle.java
+++ core/src/main/java/aritzh/libgdx/game1/core/util/Rectangle.java
@@ -5,4 +5,53 @@ package aritzh.libgdx.game1.core.util;
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 public class Rectangle {
+
+    private final int x, y, width, height;
+
+    public Rectangle() {
+        this(0, 0, 0, 0);
+    }
+
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(int width, int height) {
+        this(0, 0, width, height);
+    }
+
+    public Rectangle translate(int dx, int dy) {
+        return new Rectangle(this.x + dx, this.y + dx, this.width, this.height);
+    }
+
+    public Rectangle setSize(int width, int height) {
+        return new Rectangle(this.x, this.y, width, height);
+    }
+
+    public Rectangle scale(int sx, int sy) {
+        return new Rectangle(this.x, this.y, this.width * sx, this.height * sy);
+    }
+
+    public boolean contains(Point point) {
+        return point.getX() >= this.x && point.getX() <= this.x + this.width && point.getY() >= this.y && point.getY() <= this.y + this.height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
