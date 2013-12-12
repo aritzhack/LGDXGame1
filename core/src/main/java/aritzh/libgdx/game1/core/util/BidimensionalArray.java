4c39526e844117630cd28363eb5febc36d3e35ae
aritzhack <aritzhack@gmail.com>
2013-12-12 16:44:15 +0100

    * Bidimensional resizable array fully functional (should be) * Bidimensional resizable array test tests correctly

--- core/src/main/java/aritzh/libgdx/game1/core/util/BidimensionalArray.java
+++ core/src/main/java/aritzh/libgdx/game1/core/util/BidimensionalArray.java
@@ -1,12 +1,15 @@
 package aritzh.libgdx.game1.core.util;
 
+import com.google.common.base.Function;
+
 import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * @author Aritz Lopez
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
-public class BidimensionalArray<E> {
+public class BidimensionalArray<E> implements Iterable<E> {
 
     private static final int DEFAULT_SIZE = 256;
 
@@ -18,22 +21,44 @@ public class BidimensionalArray<E> {
     }
 
     public BidimensionalArray(int width, int height) {
+        this.width = closestBiggerPorwerOfTwo(width);
+        this.height = closestBiggerPorwerOfTwo(height);
         this.matrix = new Object[width][height];
-        this.width = width;
-        this.height = height;
     }
 
     public void set(int x, int y, E value) {
+        //noinspection StatementWithEmptyBody
         while(this.checkSize(x, y));
         try {
             this.matrix[x][y] = value;
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.err.println("Error accessing index " + x + ", " + y);
+            System.err.println("Error setting index " + x + ", " + y);
+            e.printStackTrace();
         }
+    }
+
+    public BidimensionalArray<Boolean> applyToAll(Function<E, Boolean> function){
+        BidimensionalArray<Boolean> ret = new BidimensionalArray<Boolean>(this.width, this.height);
+
+        for(int x = 0; x<this.width; x++){
+            for(int y = 0; y<this.height; y++){
+                ret.set(x, y, function.apply(this.get(x, y)));
+            }
+        }
+        return ret;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int x, int y){
+        if(x >= this.width || y>= this.height) return null;
+        return (E) this.matrix[x][y];
     }
 
     private boolean checkSize(int width, int height) {
         if (this.width > width && this.height > height) return false;
+
+        int lastWidth = this.width;
+        int lastHeight = this.height;
 
         if (this.width <= width) {
             this.width *= 2;
@@ -43,14 +68,62 @@ public class BidimensionalArray<E> {
             this.height *= 2;
         }
 
-        Object[][] temp = this.matrix;
-        this.matrix = new Object[this.width][this.height];
-        System.arraycopy(temp, 0, this.matrix, 0, temp.length);
+        this.matrix = Arrays.copyOf(this.matrix, this.width);
+
+        for(int i = 0; i<this.width; i++){
+            if(i<lastWidth) this.matrix[i] = Arrays.copyOf(this.matrix[i], this.height);
+            else this.matrix[i] = new Object[this.height];
+        }
         return true;
+    }
+
+    private static int closestBiggerPorwerOfTwo(int number){
+        return 32 - Integer.numberOfLeadingZeros(number - 1);
     }
 
     @Override
     public String toString() {
         return Arrays.deepToString(this.matrix);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new BidimensionalArrayIterator();
+    }
+
+    private class BidimensionalArrayIterator implements Iterator<E> {
+
+        int currX, currY;
+        BidimensionalArray<E> array = BidimensionalArray.this;
+
+        @Override
+        public boolean hasNext() {
+            return !(this.currX+1 < BidimensionalArray.this.getWidth() && this.currY+1 < BidimensionalArray.this.getHeight());
+        }
+
+        @Override
+        public E next() {
+            if(!this.hasNext()) return null;
+            this.currX++;
+            if(this.currX == array.getWidth()){
+                this.currX = 0;
+                this.currY++;
+            }
+
+            return array.get(this.currX, this.currY);
+        }
+
+        @Override
+        public void remove() {
+            array.set(this.currX, this.currY, null);
+        }
     }
 }
--- core/src/test/java/aritzh/libgdx/game1/core/util/BidimensionalArrayTest.java
+++ core/src/test/java/aritzh/libgdx/game1/core/util/BidimensionalArrayTest.java
@@ -1,17 +1,52 @@
 package aritzh.libgdx.game1.core.util;
 
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Random;
+
 /**
  * @author Aritz Lopez
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 public class BidimensionalArrayTest {
 
-    @org.junit.Test
+    private static final int TIMES = 100000000, MAX_SIZE = 8191;
+
+    @Test
     public void testToString() throws Exception {
         BidimensionalArray<Integer> bidiArray = new BidimensionalArray<Integer>(10, 10);
+        List<Entry> entries = Lists.newArrayList();
+        Random r = new Random();
 
-        bidiArray.set(100, 100, 50);
+        for(int i = 0; i<TIMES; i++){
+            int x = r.nextInt(MAX_SIZE);
+            int y = r.nextInt(MAX_SIZE);
 
-        System.out.println(bidiArray);
+            if(bidiArray.get(x, y) == null) continue;
+
+            int value = r.nextInt();
+
+            entries.add(new Entry(x, y, value));
+            bidiArray.set(x, y, value);
+        }
+
+        for (Entry entry : entries){
+            assert bidiArray.get(entry.x, entry.y) == entry.value;
+        }
+    }
+
+    private static class Entry {
+
+        public final int x;
+        public final int y;
+        public final int value;
+
+        public Entry(int x, int y, int value){
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
     }
 }
